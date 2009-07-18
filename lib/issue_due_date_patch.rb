854ac28fd4b27c960821cee6cf0da1bd8e402ac2
Eric Davis <edavis@littlestreamsoftware.com>
2009-07-18 15:18:54 -0700

    [#2784] Refactored Issue#due_date_set_by_version?

--- lib/issue_due_date_patch.rb
+++ lib/issue_due_date_patch.rb
@@ -78,12 +78,11 @@ module IssueDueDate
 
       # Is the issue's +due_date+ the same as it's old version?
       def due_date_set_by_version?
-        orig_issue = Issue.find_by_id(self.id) || Issue.new
-        return false unless orig_issue.fixed_version
-        orig_date = orig_issue.fixed_version.due_date
-        orig_date ||= nil
-        
-        return orig_date == self.due_date
+        orig_issue = Issue.find_by_id(self.id)
+
+        return !!(orig_issue &&
+                  orig_issue.fixed_version &&
+                  orig_issue.fixed_version.due_date == self.due_date)
       end
       
       # Is the issue's +due_date+ the same as it's old deliverable?
