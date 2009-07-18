d0529bdb8b39fff60c551f28467a17f0cc3119ca
Eric Davis <edavis@littlestreamsoftware.com>
2009-07-18 14:48:06 -0700

    [#2784] Removed used code

--- lib/version_due_date_patch.rb
+++ lib/version_due_date_patch.rb
@@ -3,24 +3,16 @@ require_dependency 'version'
 module IssueDueDate
   module VersionPatch
     def self.included(base)
-      base.extend(ClassMethods)
-
       base.send(:include, InstanceMethods)
 
       # Same as typing in the class 
       base.class_eval do
         unloadable # Send unloadable so it will not be unloaded in development
-        
         before_save :update_issue_due_dates
-        
       end
     end
-    
-    module ClassMethods
-    end
-    
+
     module InstanceMethods
-      
       # Added as a +before_save+ to push out the updated due_date to the issues
       def update_issue_due_dates
         self.fixed_issues.each do |issue|
